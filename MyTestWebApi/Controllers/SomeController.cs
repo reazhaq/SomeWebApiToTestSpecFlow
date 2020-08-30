e0cebd663179d8298389b2b9e5884e32cbfc98a9
reaz haq <reaz.haq@outlook.com>
2020-08-30 15:29:05 -0500

    init commit

---
+++ MyTestWebApi/Controllers/SomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyTestWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get(string value)
+        {
+            var result = $"{value} was the param";
+            return Ok(result);
+        }
+    }
+}
